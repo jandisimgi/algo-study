c69386f5ed64f0e485aeacf37aee58a5ae102e3b
tngml <158024944+tngml@users.noreply.github.com>
2025-07-20 16:43:06 +0900

    Create BOJ_14501_퇴사.java

---
+++ Algo_250714/suhee/BOJ_14501_퇴사.java
@@ -0,0 +1,33 @@
+package 백준알고리즘;
+
+import java.util.Scanner;
+
+public class B_14501_퇴사 {
+
+
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        int N = sc.nextInt();
+	        int[] T = new int[N + 1];
+	        int[] P = new int[N + 1]; 
+	        int[] dp = new int[N + 2]; 
+
+	        for (int i = 1; i <= N; i++) {
+	            T[i] = sc.nextInt();
+	            P[i] = sc.nextInt();
+	        }
+
+	        for (int i = N; i >= 1; i--) {
+	            if (i + T[i] <= N + 1) {
+	                dp[i] = Math.max(P[i] + dp[i + T[i]], dp[i + 1]);
+	            } else {
+	                dp[i] = dp[i + 1]; 
+	            }
+	        }
+
+	        System.out.println(dp[1]);
+	    }
+	}
+
