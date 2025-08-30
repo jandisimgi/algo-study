819fe584305a38c31b41e2ecdfeec957e5a3ce83
thdus <107402806+thdus@users.noreply.github.com>
2025-08-30 23:22:49 +0900

    Update BOJ_11724_연결요소의개수.java

--- Algo_250825/soyeon/BOJ_11724_연결요소의개수.java
+++ Algo_250825/soyeon/BOJ_11724_연결요소의개수.java
@@ -2,47 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+   static int parent[];
 
-        List<Integer>[] graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            graph[u].add(v);
-            graph[v].add(u);
-        }
+      int N = Integer.parseInt(st.nextToken()); 
+      int M = Integer.parseInt(st.nextToken()); 
 
-        for (int i = 1; i <= N; i++) Collections.sort(graph[i]);
+      parent = new int[N + 1];
+      for (int i = 1; i <= N; i++) {
+         parent[i] = i;
+      }
 
-        boolean[] visited = new boolean[N + 1];
-        int count = 0;
+      for (int i = 0; i < M; i++) {
+         st = new StringTokenizer(br.readLine());
+         int u = Integer.parseInt(st.nextToken());
+         int v = Integer.parseInt(st.nextToken());
+         union(u, v);
+      }
 
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                count++;
-                Deque<Integer> stack = new ArrayDeque<>();
-                stack.push(i);
-                visited[i] = true;
+    
+      int cnt = 0;
+      boolean check[] = new boolean[N + 1]; 
+      for (int i = 1; i <= N; i++) {
+         int root = find(parent[i]);
+         if (!check[root]) {
+            check[root] = true;
+            cnt++;
+         }
+      }
+      System.out.println(cnt);
+      
+      
+   static void union(int a, int b) {
+      int rootA = find(a);
+      int rootB = find(b);
+      if (rootA != rootB) { 
+         parent[rootB] = rootA; 
+      }
+   }
 
-                while (!stack.isEmpty()) {
-                    int cur = stack.pop();
-                    for (int next : graph[cur]) {
-                        if (!visited[next]) {
-                            visited[next] = true;
-                            stack.push(next);
-                        }
-                    }
-                }
-            }
-        }
 
-        System.out.println(count);
-    }
+   static int find(int x) {
+      if (x != parent[x])
+         parent[x] = find(parent[x]); 
+      return parent[x];
+   }
 }
