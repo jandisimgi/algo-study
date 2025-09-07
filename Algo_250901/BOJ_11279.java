dddbd1f2bb110316161c46d257e24d3e262d137c
kyngmn <79073979+kyngmn@users.noreply.github.com>
2025-09-08 01:38:02 +0900

    Add files via upload

---
+++ Algo_250901/BOJ_11279.java
@@ -0,0 +1,25 @@
+
+// 최대 힙
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ_11279 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // 1 ≤ N ≤ 100,000
+		PriorityQueue<Integer> pq = new PriorityQueue<>((Integer a, Integer b) -> b - a);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			int input = Integer.parseInt(br.readLine());
+			pq.offer(input);
+			if (input == 0) {
+				sb.append(pq.poll());
+				sb.append("\n");
+			}
+
+		}
+		System.out.println(sb);
+	}
+}
