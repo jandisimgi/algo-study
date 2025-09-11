4490acdb383bea4ad2424954cf221dad068e4cc7
tngml <158024944+tngml@users.noreply.github.com>
2025-09-11 09:38:37 +0900

    Create BOJ_7576_토마토.java

---
+++ Algo_250908/suhee/BOJ_7576_토마토.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static int M, N;              
+    static int[][] map;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken()); 
+        N = Integer.parseInt(st.nextToken()); 
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 1) {
+                	q.add(new int[]{i, j});
+                }
+            }
+        }
+
+
+        bfs(q);
+
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
+                }
+                max = Math.max(max, map[i][j]);
+            }
+        }
+        System.out.println(max - 1); 
+    }
+
+
+    private static void bfs(Queue<int[]> q) {
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+
+
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+
+
+                if (map[nr][nc] != 0) continue;
+
+
+                map[nr][nc] = map[r][c] + 1;
+                q.add(new int[]{nr, nc});
+            }
+        }
+    }
+}
